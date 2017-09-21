0d4323be55185dafa9c89491ec9b23eae3ebec22
Ricardo Lousada <ricardo.lousada@gmail.com>
2017-09-21 20:04:40 +0100

    Initial Commit

---
+++ TestProjectToSetUpGitHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProjectToSetUpGitHub
+//
+//  Created by Ricardo Lousada on 21/09/17.
+//  Copyright © 2017 IT-4-ALL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
